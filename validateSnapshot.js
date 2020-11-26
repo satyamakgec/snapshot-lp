c611dfd6aa4f178f06e84c22c36c03cd939664bb
satyam <satyam@secureblocks.io>
2020-11-26 17:12:49 +0530

    fix archive issue

--- validateSnapshot.js
+++ validateSnapshot.js
@@ -6,8 +6,22 @@ const csvParse = require('csv-parse/lib/sync');
 const fs = require("fs");
 
 const SECONDS_IN_A_DAY = 86400;
+const ABI_PATH = "./artifacts/UniswapV2Pair.json";
 const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.MAINNET_CONNECTOR));
 let validHolders = new Array();
+
+let abi;
+
+// Read the ABI for UniswapV2Pair contract.
+// If successful read then process ahead otherwise exit.
+try {
+    if (fs.existsSync(ABI_PATH)) {
+        abi = JSON.parse(fs.readFileSync(ABI_PATH, "utf8"));
+    }
+}catch(e) {
+    console.log(`Error in reading the ABI of contract: ${e.error}`);
+    process.exit(1); // Forcefully exit.
+}
 
 async function validateSnapshot(path, pairAddress) {
     let inputContent;
@@ -15,7 +29,7 @@ async function validateSnapshot(path, pairAddress) {
         if (fs.existsSync(path)) {
             inputContent = fs.readFileSync(path, "utf8");
         }
-    }catch(e) {
+    } catch(e) {
         console.log(`Error in reading the ABI of contract: ${e.error}`);
         process.exit(1); // Forcefully exit.
     }
@@ -23,27 +37,22 @@ async function validateSnapshot(path, pairAddress) {
         columns: true,
         skip_empty_lines: true
     });
+
+    // Create the instance of the UniswapV2Pair contract.
+    let pairInstance = new web3.eth.Contract(abi, pairAddress);
+
     let currentTime = parseInt(timestamp.now());
     if (!(currentTime - parseInt(data[0].timestamp) >= process.env.VALIDATION_PERIOD_IN_DAYS * SECONDS_IN_A_DAY)) {
         console.log("Reward period is still left");
         process.exit(1);
     } else {
-        let currentBlockNumber = await web3.eth.getBlockNumber();
-        let periodInterval = parseInt(process.env.VALIDATE_BLOCK_INTERVAL);
-        let startBlockNumber = parseInt(data[0].blockNumber);
-        let noOfPeriods = (currentBlockNumber - startBlockNumber)/ periodInterval;
+        let promises = [];
         for(let i = 0; i < data.length; i++) {
-            let nextBlockNumber = startBlockNumber;
-            let counter = 0;
-            for(let j = 0; j < noOfPeriods; j++) {
-                nextBlockNumber += periodInterval;
-                if (await balanceOfInvestor(pairAddress, data[0].holder, nextBlockNumber) < data[i].amount) {
-                    break;
-                }
-                counter++;
-            }
-            if (counter == noOfPeriods) {
-                validHolders.push(data[i]);
+            if (await isHolderStillValid(pairInstance, "Transfer", data[i].blockNumber, data[i].holder, data[i].amount)) {
+                validHolders.push({
+                    "holder": data[i].holder,
+                    "balance": data[i].amount,
+                });
             }
         }
     }
@@ -52,16 +61,34 @@ async function validateSnapshot(path, pairAddress) {
     process.exit(0);
 }
 
-function encodeUint(data) {
-    return web3.eth.abi.encodeParameter('uint256', data);
-}
-
-async function balanceOfInvestor(contractAddress, target, blockNumber) {
-    let index = encodeUint(1);
-    let key = web3.eth.abi.encodeParameter('address', target);
-    var tempKey = key + index.substring(2);
-    let slot = encodeUint(web3.utils.sha3(tempKey, {"encoding": "hex"}));
-    return parseInt((web3.utils.toBN(await web3.eth.getStorageAt(contractAddress, slot, blockNumber))).toString());
+async function isHolderStillValid(pairInstance, eventType, fromBlock, holder, snappedHolding) {
+    let currentHolding = await pairInstance.methods.balanceOf(holder).call();
+    if (currentHolding < snappedHolding) {
+        return (false);
+    }
+    // Read all the Transfer event and Mint event to know the token holders.
+    let events = await pairInstance.getPastEvents(eventType, {
+        fromBlock: fromBlock,
+        toBlock: 'latest'
+    });
+    if (events.length > 0 ) {
+        let newBalance = parseInt(snappedHolding);
+        let valid = true;
+        for(let i = 0; i < events.length; i++) {
+            let temp = events[i].returnValues;
+            if (temp["to"] == holder) {
+                newBalance += parseInt(temp["value"]);
+            } else if (temp["from"] == holder) {
+                newBalance -= temp["value"];
+                if (newBalance < snappedHolding) {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+        return (valid);
+    }
+    return (true);
 }
 
 validateSnapshot("./dataset/test.csv", process.env.PAIR_ETH);
